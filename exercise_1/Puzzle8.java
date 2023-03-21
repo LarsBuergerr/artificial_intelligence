fe8f38817f168af8af56e9ae0133e0db55eacb50
Lars Buerger <lars.buerger@freenet.de>
2023-03-21 14:28:53 +0100

    added parity function

--- exercise_1/Puzzle8.java
+++ exercise_1/Puzzle8.java
@@ -10,6 +10,7 @@ public class Puzzle8 {
 	
 	public static void main(String[] args) {
 		Board b = new Board(); // Loesbares Puzzle b zufällig genrieren.
+		System.out.println(b.parity());
 		System.out.println(b);
 
 		Deque<Board> res = A_Star.aStar(b);
