1083321ce3721a8d9be90312dc8b8c88e6f4c6e5
haasentimo <haasentimo@gmail.com>
2023-03-24 22:55:05 +0100

    implemented dfs()

--- src/puzzle8/IDFS.java
+++ src/puzzle8/IDFS.java
@@ -1,35 +1,52 @@
 package puzzle8;
 
 import java.util.Deque;
-import java.util.HashSet;
 import java.util.LinkedList;
 
 /**
  * Klasse IDFS für iterative deepening depth-first search
+ *
  * @author Ihr Name
  */
 public class IDFS {
 
-	private static Deque<Board> dfs(Board curBoard, Deque<Board> path, int limit) {
-		if(limit == 0) {
-			return null;
-		}
-		return null;
-	}
-	
-	private static Deque<Board> idfs(Board curBoard, Deque<Board> path) {
-		for (int limit = 5; limit < Integer.MAX_VALUE; limit++) {
-			Deque<Board> result = dfs(curBoard,path,limit);
-			if (result != null)
-				return result;
-		}
-		return null;
-	}
-	
-	public static Deque<Board> idfs(Board curBoard) {
-		Deque<Board> path = new LinkedList<>();
-		path.addLast(curBoard);
-		Deque<Board> res =  idfs(curBoard, path); 
-		return res;
-	}
+    private static Deque<Board> dfs(Board curBoard, Deque<Board> path, int limit) {
+        if (curBoard.isSolved()) {
+            return path;
+        } else if (limit == 0) {
+            return null;
+        } else {
+            boolean cutOffOccurred = false;
+            for (Board child : curBoard.possibleActions()) {
+                if (path.contains(child)) {
+                    continue;
+                }
+                path.add(child);
+                Deque<Board> result = dfs(child, path, limit - 1);
+                if (result == null) {
+                    cutOffOccurred = true;
+                } else if (result.getLast().isSolved()) {
+                    return result;
+                }
+                path.removeLast();
+            }
+            return null;
+        }
+    }
+
+    private static Deque<Board> idfs(Board curBoard, Deque<Board> path) {
+        for (int limit = 5; limit < Integer.MAX_VALUE; limit++) {
+            Deque<Board> result = dfs(curBoard, path, limit);
+            if (result != null)
+                return result;
+        }
+        return null;
+    }
+
+    public static Deque<Board> idfs(Board curBoard) {
+        Deque<Board> path = new LinkedList<>();
+        path.addLast(curBoard);
+        Deque<Board> res = idfs(curBoard, path);
+        return res;
+    }
 }
--- src/puzzle8/Puzzle8.java
+++ src/puzzle8/Puzzle8.java
@@ -11,13 +11,14 @@ public class Puzzle8 {
 	public static void main(String[] args) {
 		Board b = new Board(); // Loesbares Puzzle b zufällig genrieren.
 		System.out.println(b);
+		while (!b.parity()) b = new Board();
 
-		Deque<Board> res = A_Star.aStar(b);
+//		Deque<Board> res = A_Star.aStar(b);
+//		int n = res == null ? -1 : res.size();
+//		System.out.println("Anz.Zuege: " + n + ": " + res);
+
+		Deque<Board> res = IDFS.idfs(b);
 		int n = res == null ? -1 : res.size();
-		System.out.println("Anz.Zuege: " + n + ": " + res);
-		
-		res = IDFS.idfs(b);
-		n = res == null ? -1 : res.size();
 		System.out.println("Anz.Zuege: " + n + ": " + res);
 	}
 }
